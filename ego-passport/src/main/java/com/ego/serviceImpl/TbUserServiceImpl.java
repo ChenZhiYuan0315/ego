cfe8a979cf4c2f4eae46a6f49bb32710b99b8aa5
ChenZhiYuan0315 <49522029+ChenZhiYuan0315@users.noreply.github.com>
2019-07-23 11:27:22 +0800

    Update TbUserServiceImpl.java

--- ego-passport/src/main/java/com/ego/serviceImpl/TbUserServiceImpl.java
+++ ego-passport/src/main/java/com/ego/serviceImpl/TbUserServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.config.annotation.Reference
 import com.commons.pojo.EgoStatus;
 import com.commons.utils.CookieUtils;
 import com.commons.utils.IDUtils;
